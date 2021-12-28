5df231d31b8b127cf7fa4f411885dd2012b41232
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2021-12-28 15:02:23 -0300

    Criando operação de instanciação

--- src/main/java/com/lucasg/cursomc/CursomcApplication.java
+++ src/main/java/com/lucasg/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.lucasg.cursomc;
 
+import com.lucasg.cursomc.domain.Categoria;
+import com.lucasg.cursomc.repositories.CategoriaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
+
 @SpringBootApplication
-public class CursomcApplication {
+@RequiredArgsConstructor
+public class CursomcApplication implements CommandLineRunner {
+
+    private final CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(List.of(cat1, cat2));
+    }
 }
