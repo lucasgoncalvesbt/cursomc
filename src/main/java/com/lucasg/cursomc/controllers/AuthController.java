aac38ebf10a21b296eef60159d191c4f8c7c9815
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-13 18:04:59 -0300

    Expondo o header Authorization (problema de Cors)

--- src/main/java/com/lucasg/cursomc/controllers/AuthController.java
+++ src/main/java/com/lucasg/cursomc/controllers/AuthController.java
@@ -33,6 +33,7 @@ public class AuthController {
         UserSS user = UserService.authenticated();
         String token = jwtUtil.generateToken(user.getUsername());
         response.addHeader("Authorization", token);
+        response.addHeader("access-control-expose-headers", "Authorization");
         return ResponseEntity.noContent().build();
     }
 
