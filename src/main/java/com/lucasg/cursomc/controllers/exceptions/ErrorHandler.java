684b40c53a6487c9ccd990fe6248a4b446462670
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-13 17:15:05 -0300

    Tratando exceções adequadamente

--- src/main/java/com/lucasg/cursomc/controllers/exceptions/ErrorHandler.java
+++ src/main/java/com/lucasg/cursomc/controllers/exceptions/ErrorHandler.java
@@ -1,7 +1,12 @@
 package com.lucasg.cursomc.controllers.exceptions;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.lucasg.cursomc.services.exceptions.AuthorizationException;
 import com.lucasg.cursomc.services.exceptions.DataIntegrityExeception;
+import com.lucasg.cursomc.services.exceptions.FileException;
 import com.lucasg.cursomc.services.exceptions.ObjectNotFoundExeception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,5 +44,30 @@ public class ErrorHandler {
     public ResponseEntity<StandardError> authorizationException(AuthorizationException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+    }
+
+    @ExceptionHandler(FileException.class)
+    public ResponseEntity<StandardError> fileException(FileException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonServiceException.class)
+    public ResponseEntity<StandardError> amazonServiceException(AmazonServiceException e, HttpServletRequest request) {
+        HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
+        StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(code).body(err);
+    }
+
+    @ExceptionHandler(AmazonClientException.class)
+    public ResponseEntity<StandardError> amazonClientException(AmazonClientException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(AmazonS3Exception.class)
+    public ResponseEntity<StandardError> amazonS3Exception(AmazonS3Exception e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/lucasg/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,11 @@
+package com.lucasg.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+    public FileException(String msg) {
+        super(msg);
+    }
+
+    public FileException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
